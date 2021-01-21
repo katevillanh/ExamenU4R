25a68956b4b38df5d1864d7d491c75afc6ae3272
Kate Villanueva <71800218+katevillanh@users.noreply.github.com>
2021-01-21 17:38:10 -0600

    Create Nodo.java

---
+++ examenu4r/Nodo.java
@@ -0,0 +1,18 @@
+
+package examenu4r;
+
+public class Nodo {
+
+	public Nodo izquierda;
+	public Nodo derecha;
+	public int elemento; 
+	public int nivel;
+				 
+	public Nodo(int elemento) {
+		this.elemento = elemento;
+		this.izquierda = null;
+		this.derecha = null;
+		this.nivel = 0;
+	}
+
+}
